ad598d3282de409ea5693482afe52e04b9b520b0
Jagadish Paul <jagadish.paul@aprodence.com>
2024-04-24 16:32:25 +0530

    Sing in view practice

---
+++ SwiftUIMapApplication/Views/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  SwiftUIMapApplication
+//
+//  Created by MSS on 24/04/24.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
